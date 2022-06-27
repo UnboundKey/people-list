5ef67445889688ba8eab0ea9cd4d44b46b893f60
Benjamin Nordin <godiset@gmail.com>
2022-06-27 09:07:18 +0200

    I've broken something

--- src/App.js
+++ src/App.js
@@ -1,50 +1,77 @@
 import './App.css';
-import PeopleRow from './components/PeopleRow';
-import PeopleTable from './components/PeopleTable';
+import React from "react"
 var requestURI = 'https://localhost:5001/api/people/all';
 const axios = require('axios');
 
-//var apiData = fetch(requestURI).then(res => res.json().then(data => console.log(data)))
 
-var data;
-axios.get(requestURI).then(function(responce) {
-  // console.log(responce)
-  data = responce.data;
-})
+//var apiData = fetch(requestURI).then(res => res.json().then(data => console.log(data)));
+
+
+//   axios.get(requestURI).then(function(responce) {
+//     // console.log(responce)
+//     //  setDBdata("responce.data");
+//  })
+
+
 
 function App() {
+
+  const [dbData, setDBdata] = React.useState();
+
+  axios.get(requestURI).then(function(responce) {
+    // console.log(responce)
+     setDBdata(responce.data);
+ })
+
   return (
   <div className="App">
       <h2>List of people</h2>
-      <div>
-      <table>
-        <thead>
-          <th>Id</th>
-          <th>Name</th>
-          <th>City</th>
-          <th>Languages</th>
-          <th>Phone Number</th>
-        </thead>
-        <tbody>
-        {data.map(peopledata => 
-            <tr key={peopledata.Id}>
-              <td>{peopledata.Id}</td>
-              <td>{peopledata.Name}</td>
-              <td>{peopledata.PersonCity.Name}</td>
-              
-              {peopledata.LanguagesLinkObject.$values.map( personLanguages => 
-               <tr><>{personLanguages.Language.Name}</></tr>
-              )}
-              <td>{peopledata.PhoneNumber}</td>
-              <td><a href='google.com'>Details</a></td>
-            </tr>
-            )}
-        </tbody>
-      </table>
-    </div>
-
+      <PeopleTable dbData={dbData}/>
     </div>
   );
+  
+}
+
+
+function sortByName() {
+  // console.log("Sorting")
+}
+
+
+
+function PeopleTable(dbData) {
+  console.log(dbData)
+  return(
+    <>
+    <table className='PeopleList'>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th onClick={sortByName()}>Name</th>
+          <th>Phone Nubmer</th>
+          <th>City</th>
+        </tr>
+      </thead>
+      <tbody>
+        <PeopleTableEntry entry={dbData[0]}/>
+      </tbody>
+    </table>
+    </>
+  )
+}
+
+
+function PeopleTableEntry(entry) {
+  // console.log(entry)
+  return (
+    <>
+     <tr>
+        <td>{entry.Id}</td>
+        <td>{entry.Name}</td>    
+     </tr>
+    </>
+
+  )
 }
 
 export default App;
