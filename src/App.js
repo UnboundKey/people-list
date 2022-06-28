075b8738c303200b4221ab5dd19f739c32cf0fd6
Benjamin Nordin <godiset@gmail.com>
2022-06-28 21:46:13 +0200

    Finally got the list to work

--- src/App.js
+++ src/App.js
@@ -1,70 +1,90 @@
 import './App.css';
 import React, { useEffect } from "react"
-var requestURI = 'https://localhost:5001/api/people/';
+import { render } from '@testing-library/react';
+var ENDPOINT = 'https://localhost:5001/api/people/';
 const axios = require('axios');
 
-
-//var apiData = fetch(requestURI).then(res => res.json().then(data => console.log(data)));
-
-
-//   axios.get(requestURI).then(function(responce) {
-//     // console.log(responce)
-//     //  setDBdata("responce.data");
-//  })
-
-
-function App() {
+ function App() {
 
   const [dbData, setDBdata] = React.useState();
+  const [Loading, setLoading] = React.useState(true);
 
-  useEffect(()=>{
-    axios.get(requestURI+"all").then(function(response) {
-        console.log(response)
-      if(response.status === "200") {
-        setDBdata(response);
+  useEffect( ()=>{
+      axios.get(ENDPOINT+"all").then(function(response) {
+      if(response.status === 200) {
+        setLoading(false);
+        setDBdata(response.data.$values);
       }
-      console.log(dbData)
+       console.log(dbData)
    })
-  },[])
-  
-
-  return (
-  <div className="App">
-      <h2>List of people</h2>
-      <PeopleTable dbData={dbData}/>
-  </div>
-  );
-  
-}
+   .catch((err) => {
+    console.log(err);
+  })
+},[])
 
 
-function sortByName() {
-  // console.log("Sorting")
-}
-
-
-
-function PeopleTable(dbData) {
-   console.log(dbData)
-
-  return(
+function PeopleTable(props) {
+   return(
     <>
     <table className='PeopleList'>
       <thead>
         <tr>
           <th>Id</th>
           <th>Name</th>
-          <th>Phone Nubmer</th>
+          <th>Phone Number</th>
           <th>City</th>
         </tr>
       </thead>
       <tbody>
-        {}
+        {props.dbData.map((data) => {
+          return (
+          <>
+          <PeopleRow person={data}/>
+          </>)
+        }) }   
       </tbody>
     </table>
     </>
   )
 }
+
+function PeopleRow(props) {
+  return(
+    <>
+      <tr>
+        <td>{props.person.Id}</td>
+        <td>{props.person.Name}</td>
+        <td>{props.person.PhoneNumber}</td>
+        <td>{props.person.City}</td>
+        <td><button>Details</button></td>
+      </tr>
+    </>
+  )
+}
+
+function PeopleActionButtons() {
+  return(
+    <>
+      <td>
+        <a href="">Delete</a>
+      </td>
+    </>
+  )
+}
+
+  const content = Loading ? <p>Loading...</p> : <PeopleTable dbData = {dbData}/>
+
+  return (
+  <div className="App">
+      <h2>List of people</h2>
+      {content}
+  </div>
+  );
+  
+}
+
+
+
 
 export default App;
 
